6281a77bc87c65aded3012bca4b88a2f41d18694
Shriyasshh Dharkar <shriyashdharkar@gmail.com>
2024-12-07 11:45:12 +0530

    File IO

--- By Code with Harry/learning python 2.py
+++ By Code with Harry/learning python 2.py
@@ -1,5 +1,5 @@
 #Global variable and local variable
-
+'''
 x=7
 print(x)
 
@@ -15,4 +15,31 @@ print(f"the global x is {x}")
 hello()
 print(f"the global x is {x}")
 # print(y)
+'''
 
+# File IO
+
+#Reading a file 
+'''f=open('myfile.txt','r')
+print(f.read())
+f.close()
+'''
+# writing a file
+'''
+f=open('myfile.txt','w')
+w=f.write("Hello World")
+print(w)
+f.close()
+'''
+
+# Append a file
+'''
+f=open('myfile.txt','a')
+w=f.write(" Hello World")
+f.close()
+'''
+
+'''
+with open('myfile.txt','a') as f:
+    f.write(" Hey I am inside with ")
+    '''
