9ad79ef21b1b61b7f443dbf4faca2940c2982536
Shriyasshh Dharkar <shriyashdharkar@gmail.com>
2024-12-09 01:18:57 +0530

    File IO functions

--- By Code with Harry/learning python 2.py
+++ By Code with Harry/learning python 2.py
@@ -43,3 +43,63 @@ f.close()
 with open('myfile.txt','a') as f:
     f.write(" Hey I am inside with ")
     '''
+
+
+#Readline()
+'''
+f=open('myfile.txt','r')
+while True:
+    line=f.readline()
+    print(line)
+    if not line:
+        break
+    print(line)
+'''
+
+
+'''
+i=0
+while True: 
+    i+=1
+    line=f.readline()
+    # print(line)
+    if not line:
+        break
+    m1=int(line.split(",")[0])
+    m2=int(line.split(",")[1])
+    m3=int(line.split(",")[2])
+
+    print(f"marks of student {i}in maths", m1*2)
+    print(f"marks of student {i}in eng", m2*2)
+    print(f"marks of student {i}in GK", m3*2)   
+'''
+
+
+#Writeline()
+
+'''
+f=open('myfile2.txt','w')
+lines=['line 1\n','line 2\n','line 3\n']
+f.writelines(lines)
+f.close()
+'''
+#seek(),tell()
+'''
+with open ('myfile.txt','r') as f:
+        print(type(f))
+
+        f.seek(5)
+        print(f.tell())
+        data=f.read(5)
+        print(data)
+'''
+
+
+#truncate()
+
+with open('myfile2.txt','w') as f:
+    f.write("Hello World")
+    f.truncate(8)
+
+with open('myfile2.txt','r') as f:
+        print(f.read())
