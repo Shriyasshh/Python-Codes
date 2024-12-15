11de7bea2ba67a26a409ccd2bef80a051a31aab9
Shriyasshh Dharkar <shriyashdharkar@gmail.com>
2024-12-15 23:46:02 +0530

    Inheritance

--- By Code with Harry/OOP.py
+++ By Code with Harry/OOP.py
@@ -80,3 +80,23 @@ obj.ten_value=67
 print(obj.ten_value)
 obj.show()
 '''
+
+# Inheritance
+
+class Employee:
+    def __init__(self,name,id):
+        self.name=name
+        self.id=id
+
+    def showDetails(self):
+        print(f"The name of Employee : {self.id} is {self.name}")
+
+class Programmer(Employee):     #Inheitance
+    def showLanguage(self):
+        print("Default language is Python")
+        print(self.name)
+e1=Employee("Bob",101)
+e1.showDetails()
+
+e2=Programmer("John mini",102)
+e2.showLanguage()
