c60cfb7b24201dc6a36a563a43fa95e6026894e1
Shriyasshh Dharkar <shriyashdharkar@gmail.com>
2024-12-21 17:08:19 +0530

    Dunder method

--- By Code with Harry/OOP.py
+++ By Code with Harry/OOP.py
@@ -267,3 +267,31 @@ print(P.name)
 print(P.id)
 print(P.lang)
 '''
+
+
+#Magic/Dunder methods
+
+'''
+class Employee1:
+    def __init__(self,name):
+        self.name=name
+    def __len__(self):
+        i=0
+        for c in self.name:
+            i+=1
+        return i
+
+    def __str__(self):
+        return (f"The name of Employee is {self.name}")
+    
+    def __repr__(self):
+        return (f"The name of Employee is {self.name} repr ")
+
+    def __call__(self):
+        print("Hey I am good")
+
+
+# e=Employee1("Bob")
+# print(e.name)
+# print(len(e))
+# '''
